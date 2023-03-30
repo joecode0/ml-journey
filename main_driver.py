25248ff6c038b035c2d06836e1ac3de777beb923
joecode0 <joecode0@outlook.com>
2023-03-30 22:18:37 +0100

    Move imports to src packages

--- main_driver.py
+++ main_driver.py
@@ -1,15 +1,7 @@
-## Universal package imports
-import sys
-import numpy as np 
-import pandas as pd
-import matplotlib.pyplot as plt
-
-## Custom package imports
-from src.challenge1_linreg_house_prices import pipeline1
-
 ## Main driver function
 def main(args):
     if args[0] == '1':
+        from src.challenge1_linreg_house_prices import pipeline1
         pipeline1(args[1])
     
     return
